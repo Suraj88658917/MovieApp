ec0220671c7567427f10d9f1304a8c68a62cde52
Suraj Saini <suraj88658917@gmail.com>
2026-04-01 11:33:26 +0530

    done setup svg and fonts

--- App.jsx
+++ App.jsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import MasterCard from "./src/assets/image/MasterCard.svg";
+
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+   <View style={styles.container}>
+      <Text style={styles.title}>App</Text>
+      <Text style={styles.text}>My Name is suraj</Text>
+      <MasterCard width={90} height={90}/> 
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 26,
+    fontFamily: "Poppins-Bold",
+  },
+  text: {
+    fontSize: 16,
+    fontFamily: "Poppins-Regular",
+  },
+});
+
